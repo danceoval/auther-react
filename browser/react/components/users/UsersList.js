7974fe6510a1033174766b8707074149f49c2754
Ben Cohen <ben.cohen@fullstackacademy.com>
2016-10-12 19:00:17 -0400

    added in dummy data for query and search

--- browser/react/components/users/UsersList.js
+++ browser/react/components/users/UsersList.js
@@ -4,31 +4,75 @@ import { Link } from 'react-router';
 
 export default ({ users, removeUser }) => (
   <div className="container">
+    {/* USER QUERY - LOOK INTO FOR FIXING */}
     <div className="user-query">
-      { /* 
-        this is where we add our forms in 
-            <user-item
-              is-form
-              model="userSearch"
-              class="list-group-item min-content"
-              glyphicon="search">
-            </user-item>
-            <user-item
-              is-form
-              model="userAdd"
-              class="list-group-item min-content"
-              glyphicon="plus"
-              icon-click="addUser()">
-            </user-item>
-      */ }
+      {/* SEARCH */}
+      <div className="list-group-item min-content user-item">
+        <div className="media">
+          <div className="media-left media-middle icon-container">
+            <span className="glyphicon glyphicon-search"></span>
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading tucked">
+              <div contentEditable
+                    placeholder="Jean Doe"
+                    className="form-like">
+              </div>
+            </h4>
+            <h5 className="tucked">
+              <div contentEditable
+                   placeholder="email@website.com"
+                   className="form-like">
+              </div>
+            </h5>
+            <h5 className="tucked">
+              <div contentEditable
+                placeholder="(555) 555-5555"
+                className="form-like">
+              </div>
+            </h5>
+          </div>
+          <div className="media-right media-middle"></div>
+        </div>
+      </div>
+      {/* ADD */}
+      <div className="list-group-item min-content user-item">
+        <div className="media">
+          <div className="media-left media-middle icon-container">
+            <span className="glyphicon glyphicon-plus clickable"></span>
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading tucked">
+              <div contentEditable
+                    placeholder="Jean Doe"
+                    className="form-like">
+              </div>
+            </h4>
+            <h5 className="tucked">
+              <div contentEditable
+                   placeholder="email@website.com"
+                   className="form-like">
+              </div>
+            </h5>
+            <h5 className="tucked">
+              <div contentEditable
+                placeholder="(555) 555-5555"
+                className="form-like">
+              </div>
+            </h5>
+          </div>
+          <div className="media-right media-middle"></div>
+        </div>
+      </div>
+    {/* END OF QUERY SEARCH */}
     </div>
     <br />
     <br />
     <div className="user-list">
     {  
       users.map((user, index) => (
-        <div key={index} className="list-group-item min-content">
-          <UserItem user={user} removeUser={removeUser} className="user-item"/>
+        <div key={index} className="list-group-item min-content user-item">
+          <UserItem user={user} removeUser={removeUser}/>
         </div> 
       )) 
     }
